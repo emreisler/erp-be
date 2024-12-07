cce85c78f00885eb6649a37acbfe0f258198da52
emreisler <emre.isler.9@gmail.com>
2024-12-07 12:15:16 +0300

    rol back

--- src/main/java/github/com/emreisler/erp_be/repo_adapter/CategoryRepoAdapter.java
+++
@@ -1,4 +0,0 @@
-package github.com.emreisler.erp_be.repo_adapter;
-
-public interface CategoryRepoAdapter {
-}
--- src/main/java/github/com/emreisler/erp_be/repository/CategoryRepository.java
+++ src/main/java/github/com/emreisler/erp_be/repository/CategoryRepository.java
@@ -2,8 +2,7 @@ package github.com.emreisler.erp_be.repository;
 
 
 import github.com.emreisler.erp_be.entity.Category;
-import github.com.emreisler.erp_be.repo_adapter.CategoryRepoAdapter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category, Long>, CategoryRepoAdapter {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
